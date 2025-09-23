93a103d4e0bc7f6d40927eee7e0e0a9b05a967cc
Roberth Parreiras <roberthparreiras@hotmail.com>
2025-09-23 18:54:37 -0300

    fix: update MySQL healthcheck command and remove MYSQL_USER environment variable

--- frontend-next/app/(public)/[id]/page.tsx
+++ frontend-next/app/(public)/[id]/page.tsx
@@ -28,31 +28,39 @@ export default function Product() {
       <Link href="/">
         <CustomButton
           type="button"
-          className="my-8 ml-4 text-xl w-1/2 sm:w-1/3 md:text-2xl"
+          className="my-8 ml-4 w-1/2 text-xl sm:w-1/3 md:text-2xl"
         >
           Back to home
         </CustomButton>
       </Link>
       <div className="mt-10 grid w-full max-w-7xl px-4 sm:grid-cols-2">
-        <Image
-          src={game?.image!}
-          alt={`${game?.name!} image`}
-          width={570}
-          height={570}
-        />
-        <div className="flex w-full flex-col justify-center gap-10 text-center">
-          <h3 className="text-3xl md:text-4xl">{game?.name!}</h3>
-          <p className="text-2xl">{game?.description!}</p>
-          <span className="text-3xl">R$ {game?.price!}</span>
-          <div className="mx-auto w-1/2 rounded-2xl bg-[#DFD0B8] py-4 text-[#393E46] hover:bg-[#948979]">
-            <Link
-              href={"#"}
-              className="w-full py-2 text-center text-3xl md:text-5xl"
-            >
-              Buy Now
-            </Link>
+        {game ? (
+          <>
+            <Image
+              src={game.image}
+              alt={`${game.name} image`}
+              width={570}
+              height={570}
+            />
+            <div className="flex w-full flex-col justify-center gap-10 text-center">
+              <h3 className="text-3xl md:text-4xl">{game.name}</h3>
+              <p className="text-2xl">{game.description}</p>
+              <span className="text-3xl">R$ {game.price}</span>
+              <div className="mx-auto w-1/2 rounded-2xl bg-[#DFD0B8] py-4 text-[#393E46] hover:bg-[#948979]">
+                <Link
+                  href={"#"}
+                  className="w-full py-2 text-center text-3xl md:text-5xl"
+                >
+                  Buy Now
+                </Link>
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="col-span-2 py-20 text-center text-2xl">
+            Loading...
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
