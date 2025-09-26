bcb2db14e86540e96ac5fb567e4c5c2c481ca41e
Roberth Parreiras <roberthparreiras@hotmail.com>
2025-09-26 07:23:21 -0300

    fix: update CI workflow trigger and set global prefix for API in main.ts

--- api_nest/src/main.ts
+++ api_nest/src/main.ts
@@ -6,6 +6,7 @@ import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.useLogger(new Logger('NestApplication', { timestamp: true }));
   app.useGlobalFilters(new HttpExceptionFilter());
   app.enableCors();
